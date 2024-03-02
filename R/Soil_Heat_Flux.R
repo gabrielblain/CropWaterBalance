ef7a221bb16ce7af8c4416d495fc35e781cc19a0
Adam H. Sparks <adamhsparks@icloud.com>
2024-03-02 17:04:05 +0800

    Lint Soil_Heat_Flux.R

--- R/Soil_Heat_Flux.R
+++ R/Soil_Heat_Flux.R
@@ -9,20 +9,22 @@
 #' @export
 #' @examples
 #' data(DataForCWB)
-#' Tavg <- DataForCWB[,2]
+#' Tavg <- DataForCWB[, 2]
 #' Soil_Heat_Flux(Tavg)
-
-Soil_Heat_Flux <- function(Tavg){
-  Tavg <-as.matrix(Tavg)
-  if(ncol(Tavg)!= 1 || any(is.na(Tavg)))
-    {stop("Tavg must be a single column variable with no missing value")}
-  n <- length(Tavg)
-  G <- matrix(NA,n,1)
-  if (n<4){stop("At least four days of Tavg are required.")}
-  for (i in 4:n){
-    G[i,1] <- 0.38*(Tavg[i]-mean(Tavg[(i-1):(i-3)]))
+Soil_Heat_Flux <- function(Tavg) {
+  Tavg <- as.matrix(Tavg)
+  if (ncol(Tavg) != 1 || any(is.na(Tavg))) {
+    stop("Tavg must be a single column variable with no missing value")
   }
-  G[1:3,1] <- 0
+  n <- length(Tavg)
+  G <- matrix(NA, n, 1)
+  if (n < 4) {
+    stop("At least four days of Tavg are required.")
+  }
+  for (i in 4:n) {
+    G[i, 1] <- 0.38 * (Tavg[i] - mean(Tavg[(i - 1):(i - 3)]))
+  }
+  G[1:3, 1] <- 0
   warning("The first 3 G values were set to zero")
   return(G)
 }
