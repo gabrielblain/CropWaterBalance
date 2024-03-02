471d0005afa2824e67fa41c407a61d5cf1aa1baf
Adam H. Sparks <adamhsparks@icloud.com>
2024-03-02 17:00:41 +0800

    Lint InitalD.R

--- R/InitialD.R
+++ R/InitialD.R
@@ -1,13 +1,17 @@
-#' Soil water deficit in the root zone
+#' Soil Water Deficit in the Root Zone
 #'
-#' Estimates initial values for soil water deficit. Required to initiate the water balance accounting.
+#' Estimates initial values for soil water deficit. Required to initiate the
+#'  water balance accounting.
 #'
 #' @param teta_FC
-#' Soil water content for the effective root zone at the field capacity \acronym{m3/m3}
+#' Soil water content for the effective root zone at the field capacity
+#'  \acronym{m3/m3}
 #' @param teta_Obs
-#' Soil water content for the effective root zone at the wilting point \acronym{m3/m3}
+#' Soil water content for the effective root zone at the wilting point
+#'  \acronym{m3/m3}
 #' @param Drz
-#' Vector, 1-column matrix or data frame defining the root zone depth in meters.
+#' Vector, 1-column matrix or data frame defining the root zone depth in
+#'  metres.
 #' @return
 #' Initial soil water deficit in the root zone (millimetres).
 #' @export
@@ -15,18 +19,17 @@
 #' teta_FC <- 0.30
 #' teta_Obs <- 0.17
 #' Drz <- 0.3048
-#' Dinitial(teta_FC=teta_FC,teta_Obs=teta_Obs,Drz=Drz)
-
-Dinitial <- function(teta_FC,teta_Obs,Drz){
-
-  if (is.numeric(teta_FC) == FALSE || length(teta_FC) != 1 ||
-      any(is.na(teta_FC)) == TRUE || teta_FC < 0 ||
-      is.numeric(teta_Obs) == FALSE || length(teta_Obs) != 1 ||
-      any(is.na(teta_Obs)) == TRUE || teta_Obs < 0 ||
-      is.numeric(Drz) == FALSE || length(Drz) != 1 ||
-      any(is.na(Drz)) == TRUE || Drz < 0 || teta_Obs > teta_FC)
-      {
+#' Dinitial(teta_FC = teta_FC, teta_Obs = teta_Obs, Drz = Drz)
+Dinitial <- function(teta_FC, teta_Obs, Drz) {
+  if (!is.numeric(teta_FC) || length(teta_FC) != 1 ||
+    any(is.na(teta_FC)) || teta_FC < 0 ||
+    !(teta_Obs) || length(teta_Obs) != 1 ||
+    any(is.na(teta_Obs)) || teta_Obs < 0 ||
+    !(Drz) || length(Drz) != 1 ||
+    any(is.na(Drz)) || Drz < 0 || teta_Obs > teta_FC) {
     stop("Physically impossible or missing teta_FC,teta_Ob or Drz values.
-         Also: teta_Obs cannot be larger than teta_FC")}
-  Dinitial <- 1000*(teta_FC - teta_Obs)*Drz
-  return(Dinitial)}
+         Also: teta_Obs cannot be larger than teta_FC")
+  }
+  Dinitial <- 1000 * (teta_FC - teta_Obs) * Drz
+  return(Dinitial)
+}
