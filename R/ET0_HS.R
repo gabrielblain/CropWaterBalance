5196d50bc4b72424c9142e672eb5f2e95a88bead
Adam H. Sparks <adamhsparks@icloud.com>
2024-03-04 08:12:54 +0800

    Clean up ROxygen code and simplify error checking

--- R/ET0_HS.R
+++ R/ET0_HS.R
@@ -3,53 +3,47 @@
 #' Calculates daily reference evapotranspiration amounts using the Hargreaves-Samani method.
 #'
 #' @param Ra
-#' Extraterrestrial solar radiation in \acronym{MJ m-2 day-1}.
+#' A `vector`, 1-column `matrix` or `data.frame` with extraterrestrial solar
+#'  radiation in \acronym{MJ m-2 day-1}.
 #' @param Tmax
-#' A vector, 1-column matrix or data frame with daily maximum air temperature
-#'  in Celsius degrees.
+#' A `vector`, 1-column `matrix` or `data.frame` with daily maximum air
+#'  temperature in Celsius degrees.
 #' @param Tmin
-#' A vector, 1-column matrix or data frame with daily minimum air temperature
-#'  in Celsius degrees.
+#' A `vector`, 1-column `matrix` or `data.frame` with daily minimum air
+#'  temperature in Celsius degrees.
 #' @param Tavg
-#' A vector, 1-column matrix or data frame with daily average air temperature.
+#' A `vector`, 1-column `matrix` or `data.frame` column with daily average air
+#'  temperature.
 #' @return
-#' A maxtrix with the daily potential evapotranspiration values in millimetres.
+#' A `matrix` of 1-column with the same length as `the input values with the
+#'  daily potential evapotranspiration values in millimetres.
 #' @export
+#' @seealso [ET0_PM()] [ET0_PT()]
 #' @examples
-#' data(DataForCWB)
+#' # See `?DataForCWB` for more on this data set
 #' Tavg <- DataForCWB[, 2]
 #' Tmax <- DataForCWB[, 3]
 #' Tmin <- DataForCWB[, 4]
 #' Ra <- DataForCWB[, 5]
 #' ET0_HS(Ra = Ra, Tavg = Tavg, Tmax = Tmax, Tmin = Tmin)
 ET0_HS <- function(Ra, Tavg, Tmax, Tmin) {
-  nRa <- length(Ra)
-  nTavg <- length(Tavg)
-  nTmax <- length(Tmax)
-  nTmin <- length(Tmin)
-  if (!is.numeric(Ra) ||
-    nRa == 0 ||
-    any(is.na(Ra)) ||
-    !is.numeric(Tavg) ||
-    nTavg == 0 ||
-    any(is.na(Tavg)) ||
-    !is.numeric(Tmax) ||
-    nTmax == 0 ||
-    any(is.na(Tmax)) ||
-    !is.numeric(Tmin) ||
-    nTmin == 0 ||
-    any(is.na(Tmin)) ||
-    nRa != nTavg || nRa != nTmax || nRa != nTmin ||
-    nTavg != nTmax || nTavg != nTmin || nTmax != nTmin) {
-    stop("Ra, Tavg, Tmax, and  Tmin must be numerical single-column variable
-          with no missing data and same length.")
+  w <- list(Ra, Tavg, Tmax, Tmin)
+  names(w) <- c("Ra", "Tavg", "Tmax", "Tmin")
+
+  if (isFALSE(any(sapply(w, is.numeric))) ||
+      any(sapply(w, length) == 0) ||
+      anyNA(w) ||
+      any(sapply(w, length) != max(sapply(w, length))) ||
+      any(sapply(w, length) != min(sapply(w, length)))) {
+    stop(
+      "`Ra`, `Tavg`, `Tmax`, and `Tmin` must be a numerical vector object with
+      no missing data and all of the same length."
+    )
   }
-  Ra <- as.matrix(Ra)
-  Tavg <- as.matrix(Tavg)
-  Tmax <- as.matrix(Tmax)
-  Tmin <- as.matrix(Tmin)
-  ET0 <- as.matrix(0.0023 * (Ra[, 1] / 2.45) *
-    (Tmax[, 1] - Tmin[, 1])^0.5 * (Tavg[, 1] + 17.8))
-  colnames(ET0) <- "ET0_HS"
+
+  ET0 <-
+    as.matrix(0.0023 * (w$Ra / 2.45) * (w$Tmax - w$Tmin) ^ 0.5 *
+                (w$Tavg + 17.8))
+  colnames(ET0) <- "ET0"
   return(ET0)
 }
