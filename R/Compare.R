dac33ff2f502215b806bdf8a9970c0abbad8f3f7
Adam H. Sparks <adamhsparks@icloud.com>
2024-03-04 17:20:46 +0800

    Lint Compare.R

--- R/Compare.R
+++ R/Compare.R
@@ -1,13 +1,15 @@
-#' Compare data from two samples
+#' Compare Data from Two Samples
 #'
 #' Calculates measures of accuracy and agreement.
 #'
 #' @param Sample1
-#' A vector, 1-column matrix or data frame with evapotranspiration or other variable.
+#' A vector, 1-column matrix or data frame with evapotranspiration or other
+#'  variable.
 #' @param Sample2
-#' A vector, 1-column matrix or data frame with evapotranspiration or other variable.
+#' A vector, 1-column matrix or data frame with evapotranspiration or other
+#'  variable.
 #' @return
-#' a \code{list} with:
+#' A \code{data.frame} with:
 #'
 #' \itemize{
 #'  \item Absolute mean error (AME),
@@ -17,38 +19,50 @@
 #'      \item original (dorig),
 #'      \item modified (dmod) and
 #'      \item refined (dref)
-#'      }
-#'  \item Pearson determination coefficient (R2), and
+#'      }, and
+#'  \item Pearson determination coefficient (RQuad).
 #'  }
 #'
 #' @examples
-#' data(DataForCWB)
-#' Tavg <- DataForCWB[,2]
-#' Tmax <- DataForCWB[,3]
-#' Tmin <- DataForCWB[,4]
-#' Rn <- DataForCWB[,6]
-#' WS <- DataForCWB[,7]
-#' RH <- DataForCWB[,8]
-#' G <- DataForCWB[,9]
-#' Sample1 <- ET0_PM(Tavg=Tavg, Tmax=Tmax, Tmin=Tmin, Rn=Rn, RH=RH, WS=WS,G=G)
-#' Sample2 <- ET0_PT(Tavg=Tavg, Rn=Rn,G=G)
-#' Compare(Sample1=Sample1, Sample2=Sample2)
+#' # See `?DataForCWB` for more on this data set
+#' Tavg <- DataForCWB[, 2]
+#' Tmax <- DataForCWB[, 3]
+#' Tmin <- DataForCWB[, 4]
+#' Rn <- DataForCWB[, 6]
+#' WS <- DataForCWB[, 7]
+#' RH <- DataForCWB[, 8]
+#' G <- DataForCWB[, 9]
+#' Sample1 <-
+#'   ET0_PM(
+#'     Tavg = Tavg,
+#'     Tmax = Tmax,
+#'     Tmin = Tmin,
+#'     Rn = Rn,
+#'     RH = RH,
+#'     WS = WS,
+#'     G = G
+#'   )
+#' Sample2 <- ET0_PT(Tavg = Tavg, Rn = Rn, G = G)
+#' Compare(Sample1 = Sample1, Sample2 = Sample2)
 #' @export
 #' @importFrom PowerSDI Accuracy
 
-Compare <- function(Sample1, Sample2){
-  if (!is.numeric(Sample1) ||
-      !is.numeric(Sample2) ||
-      length(Sample1) < 5 ||
-      length(Sample1) != length(Sample2) ||
-      any(is.na(Sample1))  ||
-      any(is.na(Sample2))) {
-    stop("Samples 1 and 2 must be numerical single column variables with at least 5 records each. Missing data are not allowed.")
-  } else{
-    ObsEst <- as.data.frame(cbind(Sample1,Sample2))
-    Comp.orig <- as.data.frame((Accuracy(obs_est = ObsEst, conf.int = "No")))
-    Comp <- as.data.frame(Comp.orig[1,3:8])
-    colnames(Comp) <- c("AME","RMSE","dorig","dmod","dref","RQuad")
+Compare <- function(Sample1, Sample2) {
+  ObsEst <- list(Sample1, Sample2)
+  if (isFALSE(any(unlist(lapply(ObsEst, is.numeric)))) ||
+  length(Sample1) < 5 ||
+  length(Sample1) != length(Sample2) ||
+  anyNA(ObsEst)) {
+    stop(
+      "Samples 1 and 2 must be numerical single column variables with at least 5
+      records each. Missing data are not allowed."
+    )
   }
+
+  Comp <-
+    as.data.frame(Accuracy(obs_est = as.data.frame(ObsEst),
+                           conf.int = "No"))[1, 3:8]
+  colnames(Comp) <-
+    c("AME", "RMSE", "dorig", "dmod", "dref", "RQuad")
   return(Comp)
 }
